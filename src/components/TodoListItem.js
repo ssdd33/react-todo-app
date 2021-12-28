f3183cbbf74520e2f7ec58ba157fbed4480023a9
LAPTOP-ICBL9CQ4\비플러스랩 <beeblu.0@gmail.com>
2021-12-28 12:49:39 +0900

    add react-virtualized

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,28 +7,38 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo: { id, text, checked }, onRemove, onToggle }) => {
+const TodoListItem = ({
+  todo: { id, text, checked },
+  onRemove,
+  onToggle,
+  style,
+}) => {
   return (
-    <div className="TodoListItem">
-      <div
-        className={cn('checkbox', { checked })}
-        onClick={() => {
-          onToggle(id);
-        }}
-      >
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div
-        className="remove"
-        onClick={() => {
-          onRemove(id);
-        }}
-      >
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => {
+            onToggle(id);
+          }}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div
+          className="remove"
+          onClick={() => {
+            onRemove(id);
+          }}
+        >
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
